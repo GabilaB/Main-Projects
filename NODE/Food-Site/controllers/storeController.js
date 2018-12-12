bf7bf2268710c1d7277d3109615e1a8316a6eba7
GabilaB <willibroad@naver.com>
2018-12-12 17:13:17 +0900

    node

--- NODE/Food-Site/controllers/storeController.js
+++ NODE/Food-Site/controllers/storeController.js
@@ -10,7 +10,37 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect("/");
+  const store = await new Store(req.body).save();
+  req.flash(
+    "success",
+    `Successfully Created ${store.name}. Care to leave a review`
+  );
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  //Query DB for al stores
+
+  const stores = await Store.find();
+  console.log(stores);
+  res.render("stores", { title: "Stores", stores });
+};
+
+exports.editStore = async (req, res) => {
+  //Find the stores by ID
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render("editStore", { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return new stores instead of old one
+    runValidators: true
+  }).exec();
+  req.flash(
+    "success",
+    `Successfully Updated <strong>${store.name}</strong> 
+    <a href="/stores/${store.slug}">. View Store </a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
 };
