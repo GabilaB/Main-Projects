6ca25fc21f8561b8e54a9633e9fe215167c683a9
GabilaB <willibroad@naver.com>
2018-12-12 20:45:21 +0900

    node

--- NODE/Food-Site/controllers/storeController.js
+++ NODE/Food-Site/controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
+const multer = require("multer");
+const jimp = require("jimp");
+const uuid = require("uuid");
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter: function(req, file, next) {
+    const isPhoto = file.mimetype.startsWith("image/");
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: "That filetype isn't allowed" }, false);
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
   res.render("index");
@@ -7,6 +22,25 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render("editStore", { title: "Add Store" });
+};
+
+exports.upload = multer(multerOptions).single("photo");
+exports.resize = async (req, res, next) => {
+  //check if there is no new file to resize
+  if (!req.file) {
+    next(); //skip to the next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split("/")[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  //now we resize
+
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+
+  //once written the photo to file system, keep going
+  next();
 };
 
 exports.createStore = async (req, res) => {
@@ -33,6 +67,8 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  //set location data to be a point
+  req.body.location.type = "Point";
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, // return new stores instead of old one
     runValidators: true
--- NODE/Food-Site/public/javascripts/modules/autocomplete.js
+++ NODE/Food-Site/public/javascripts/modules/autocomplete.js
@@ -5,9 +5,15 @@ function autocomplete(input, latInput, lngInput) {
 
   dropdown.addListener("place_changed", () => {
     const place = dropdown.getPlace();
-    // latInput.value = place.geometry.location.lat();
-    // lngInput.value = place.geometry.location.lng();
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
     console.log(place);
+  });
+
+  //if someone hits enter on the address field, don't submit form
+
+  input.on("keydown", e => {
+    if (e.keyCode === 13) e.preventDefault();
   });
 }
 
