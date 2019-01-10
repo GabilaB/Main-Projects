dbd5ae6c178a4dab6097acba6ed4d72489919868
GabilaB <willibroad@naver.com>
2019-01-10 17:25:07 +0900

    a

--- JOTER/config/database.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = {
-    mongoURI: "mongodb://<gabis>:<water1>@ds253094.mlab.com:53094/joter-k"
-  };
-} else {
-  module.exports = { mongoURI: "mongodb://localhost:27017/joters" };
-}
