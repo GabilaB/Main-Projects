a533b9d2856316de598fc5b66e4ba92b3a1868d5
GabilaB <willibroad@naver.com>
2019-01-14 16:07:19 +0900

    react

---
+++ ADD-Card-React/robofriends/src/SearchBox.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import CardList from "./CardList";
+// import SearchBox from "./SearchBox";
+// import { robots } from "./robots";
+
+const SearchBox = () => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="Search"
+        placeholder="Search bots"
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
