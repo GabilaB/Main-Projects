0e86027efc2c854d54a9d0894dd4369680cd351a
GabilaB <willibroad@naver.com>
2019-01-14 17:46:40 +0900

    react

--- ADD-Card-React/robofriends/src/App.js
+++ ADD-Card-React/robofriends/src/App.js
@@ -1,24 +1,47 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import Scroll from "./Scroll.js";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
+  }
+
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+  };
+
   render() {
-    return (
-      <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={this.state.robots} />
-      </div>
-    );
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc">Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
--- ADD-Card-React/robofriends/src/SearchBox.js
+++ ADD-Card-React/robofriends/src/SearchBox.js
@@ -3,13 +3,14 @@ import React from "react";
 // import SearchBox from "./SearchBox";
 // import { robots } from "./robots";
 
-const SearchBox = () => {
+const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className="pa2">
       <input
         className="pa3 ba b--green bg-lightest-blue"
         type="Search"
         placeholder="Search bots"
+        onChange={searchChange}
       />
     </div>
   );
