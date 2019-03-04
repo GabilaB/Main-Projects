04692631c929fc07b6d84c04622df94bacb37210
GabilaB <willibroad@naver.com>
2019-03-04 13:39:16 +0900

    pdf-view

---
+++ PDF-READER/JS/main.js
@@ -0,0 +1,50 @@
+const url = "./docs/thesis.pdf";
+
+let pdfDoc = null,
+  pageNum = 1,
+  pageIsRendering = false,
+  pageNumIsPending = null;
+
+const scale = 1.5,
+  canvas = document.querySelector("#pdf-render"),
+  ctx = canvas.getContext("2d");
+
+// Render Page
+
+const renderPage = num => {
+  pageIsRendering = true;
+
+  //Get page
+
+  pdfDoc.getPage(num).then(page => {
+    // Set scale
+
+    const viewport = page.getViewport({ scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderCtx = {
+      canvasContext: ctx,
+      viewport
+    };
+
+    page.render(renderCtx).promise.then(() => {
+      pageIsRendering = false;
+      if (pageNumIsPending !== null) {
+        renderPage(pageNumIsPending);
+        pageNumIsPending = null;
+      }
+    });
+
+    // Output Cirrent Page
+
+    document.querySelector("#page-num").textContent = num;
+  });
+};
+
+//Get Document
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+  pdfDoc = pdfDoc_;
+  document.querySelector("#page-count").textContent = pdfDoc.numPages;
+  renderPage(pageNum);
+});
