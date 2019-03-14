c17d31b559873d3930d55a7a43f450aaf46a16cb
GabilaB <willibroad@naver.com>
2019-03-14 17:13:04 +0900

    mosh

---
+++ REACT TUTO/Mosh/counter-app/src/components/counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class counter extends Component {
+  state = {
+    // count: 3,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  handleIncrement() {
+    console.log("clicked");
+  }
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className={"btn btn-secondary btn-sm"}
+        >
+          Increment
+        </button>
+        <ul />
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default counter;
