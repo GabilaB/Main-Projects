fc7b41f1da4379976df54e8fb4af70fbae98a289
GabilaB <willibroad@naver.com>
2019-04-09 15:12:30 +0900

    reactMosh

--- REACT TUTO/Mosh/counter-app/src/components/counter.jsx
+++ REACT TUTO/Mosh/counter-app/src/components/counter.jsx
@@ -1,45 +1,50 @@
 import React, { Component } from "react";
 
 class counter extends Component {
-  state = {
-    // count: 3,
-    count: this.props.value
-  };
+  // state = {
+  //   // count: 3,
+  //   value: this.props.counter.value
+  // };
 
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  handleIncrement = () => {
-    // console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  };
+  // handleIncrement = () => {
+  //   // console.log(product);
+  //   this.setState({ value: this.state.value + 1 });
+  // };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className={"btn btn-secondary btn-sm"}
         >
           Increment
         </button>
-        <ul />
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
